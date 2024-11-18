80ca5423f352502c929d8494026af65a7137d342
Saniya Dighe <saniyaps1602@gmail.com>
2024-11-18 22:30:58 +0530

    Updates

---
+++ precache-manifest.e5d8e00ab03b1a08ea14d6528ccedf6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe40fa15c4b0ff79b238",
+    "url": "/encryptiontest/static/css/main.870f9b51.chunk.css"
+  },
+  {
+    "revision": "fe40fa15c4b0ff79b238",
+    "url": "/encryptiontest/static/js/main.fe40fa15.chunk.js"
+  },
+  {
+    "revision": "60c562c18700600263fd",
+    "url": "/encryptiontest/static/js/1.60c562c1.chunk.js"
+  },
+  {
+    "revision": "05649bf2e75ac6904aba",
+    "url": "/encryptiontest/static/js/runtime~main.05649bf2.js"
+  },
+  {
+    "revision": "4f941a7c4edf0cfb8025121884207a46",
+    "url": "/encryptiontest/index.html"
+  }
+];

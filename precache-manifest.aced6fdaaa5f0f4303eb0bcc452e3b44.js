1a349cbcaddb2081cc0be5601ccf13606038ef05
Aditi <chatapp612@gmail.com>
2024-11-18 10:10:49 +0530

    Updates

---
+++ precache-manifest.aced6fdaaa5f0f4303eb0bcc452e3b44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4d4f13fa3ea6155fc4",
+    "url": "/encryptiontest/static/css/main.2ac7d8f7.chunk.css"
+  },
+  {
+    "revision": "be4d4f13fa3ea6155fc4",
+    "url": "/encryptiontest/static/js/main.be4d4f13.chunk.js"
+  },
+  {
+    "revision": "384b2eeabadd34c0cb2b",
+    "url": "/encryptiontest/static/js/1.384b2eea.chunk.js"
+  },
+  {
+    "revision": "05649bf2e75ac6904aba",
+    "url": "/encryptiontest/static/js/runtime~main.05649bf2.js"
+  },
+  {
+    "revision": "587ca09a68f2dd8b483df50a360fa513",
+    "url": "/encryptiontest/index.html"
+  }
+];

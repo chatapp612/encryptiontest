73058650d85530133ac8b34793a6338f20946949
Aditi <chatapp612@gmail.com>
2024-11-22 11:06:27 +0530

    Updates

---
+++ precache-manifest.b0762f01873c6ad402a8b50919b47d3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c5067ba91772b159e20",
+    "url": "/encryptiontest/static/css/main.d2507c6a.chunk.css"
+  },
+  {
+    "revision": "2c5067ba91772b159e20",
+    "url": "/encryptiontest/static/js/main.2c5067ba.chunk.js"
+  },
+  {
+    "revision": "eeacd89c7411685077dc",
+    "url": "/encryptiontest/static/js/1.eeacd89c.chunk.js"
+  },
+  {
+    "revision": "05649bf2e75ac6904aba",
+    "url": "/encryptiontest/static/js/runtime~main.05649bf2.js"
+  },
+  {
+    "revision": "9a287fe279e44b847aa0dc13bc7fdff4",
+    "url": "/encryptiontest/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/encryptiontest/precache-manifest.8990dd3decd579f46410c4bba30cac7e.js"
+  "/encryptiontest/precache-manifest.b0762f01873c6ad402a8b50919b47d3e.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/encryptiontest/index.html"), {
+workbox.routing.registerNavigationRoute("/encryptiontest/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
